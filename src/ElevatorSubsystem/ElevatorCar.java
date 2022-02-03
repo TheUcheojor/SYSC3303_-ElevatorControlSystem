c304b25ed707561c991a62cb9488bf094836e1d5
Ryan <rblf100@gmail.com>
2022-02-03 18:25:08 -0500

    feat: add Elevator skeleton

---
+++ src/ElevatorSubsystem/ElevatorCar.java
@@ -0,0 +1,21 @@
+package ElevatorSubsystem;
+
+/**
+ * Entity representing the elevator car
+ * 
+ * @author Ryan Fife
+ *
+ */
+
+public class ElevatorCar {
+	private int id;
+	
+	ElevatorCar(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+}
