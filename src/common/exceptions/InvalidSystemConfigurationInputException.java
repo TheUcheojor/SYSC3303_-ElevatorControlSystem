865d0bc91bdc227b99e8cc30729dffe3f63f7400
Paul Okenne <okennepaul@gmail.com>
2022-02-08 23:06:46 -0500

    feat: add new exception for invalid configuration values

---
+++ src/common/exceptions/InvalidSystemConfigurationInputException.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package common.exceptions;
+
+/**
+ * This class represents an exception where the system configuration input is
+ * invalid.
+ *
+ * @author paulokenne
+ *
+ */
+public class InvalidSystemConfigurationInputException extends Exception {
+
+	public InvalidSystemConfigurationInputException(String message) {
+		super(message);
+	}
+}
