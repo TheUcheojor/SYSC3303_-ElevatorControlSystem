bcd3b06bb2f0ddacbf30b4430ca2c7a05ef6b00d
Paul Okenne <okennepaul@gmail.com>
2022-02-13 21:57:09 -0500

    feat: add support for multiple floors

---
+++ src/FloorSubsystem/LampButton.java
@@ -0,0 +1,70 @@
+/**
+ *
+ */
+package FloorSubsystem;
+
+import common.Direction;
+
+/**
+ * The class represents a button that turns on a lamp
+ *
+ * @author paulokenne
+ *
+ */
+public class LampButton {
+
+	/**
+	 * The status of the floor lamp
+	 */
+	private boolean isButtonPressed = false;
+
+	/**
+	 * The status of the direction lamp
+	 */
+	private boolean isDirectionLampActive = false;
+
+	/**
+	 * The button direction
+	 */
+	private Direction direction;
+
+	/**
+	 * A LampButton constructor.
+	 */
+	public LampButton(Direction direction) {
+		this.direction = direction;
+	}
+
+	/**
+	 * Press the button
+	 */
+	public void press() {
+		isButtonPressed = true;
+		isDirectionLampActive = true;
+	}
+
+	/**
+	 * Reset the button
+	 */
+	public void reset() {
+		isButtonPressed = false;
+		isDirectionLampActive = false;
+	}
+
+	/**
+	 * Return a flag indicating whether the button has been pressed
+	 *
+	 * @return a flag indicating whether the button has been pressed
+	 */
+	public boolean isButtonPressed() {
+		return isButtonPressed;
+	}
+
+	/**
+	 * @return the direction
+	 */
+	public Direction getDirection() {
+		return direction;
+	}
+
+}
