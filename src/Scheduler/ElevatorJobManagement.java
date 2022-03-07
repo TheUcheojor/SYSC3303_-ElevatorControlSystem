5463ddf06147777569a4c20c8a3c3d53c6c2af0a
Paul Okenne <okennepaul@gmail.com>
2022-03-07 16:43:39 -0500

    refact: add docs

--- src/Scheduler/ElevatorJobManagement.java
+++ src/Scheduler/ElevatorJobManagement.java
@@ -6,72 +6,114 @@ import common.Direction;
 import common.messages.ElevatorJobMessage;
 
 /**
- * 
+ * This class represents the job management for an elevator.
+ *
  * @author bamideleoluwayemi
  *
  */
 public class ElevatorJobManagement {
-	
-	private ArrayList<ElevatorJobMessage> elevatorJobs= new ArrayList<>();
-	
-	private int currentFloorNumber;
-	
-	private Direction elevatorDirection;
-	
-	private int elevatorId;
-	
-	private Exception errorState;
-	
-	private boolean readyForJob;
-	
+
+	/**
+	 * The max job that a elevator can manage
+	 */
 	public static int ELEVATOR_JOB_THRESHOLD = 5;
-	
-	
+
+	/**
+	 * The elevator's jobs
+	 */
+	private ArrayList<ElevatorJobMessage> elevatorJobs = new ArrayList<>();
+
+	/**
+	 * The elevator's current floor
+	 *
+	 * Our system assumes that at start up, elevators begin at the ground floor
+	 */
+	private int currentFloorNumber = 0;
+
+	/**
+	 * The elevator's directon
+	 */
+	private Direction elevatorDirection = Direction.IDLE;
+
+	/**
+	 * The elevator's id
+	 */
+	private int elevatorId;
+
+	/**
+	 * The error state
+	 */
+	private Exception errorState = null;
+
+	/**
+	 * A flag indicating if the elevator is running a job
+	 */
+	private boolean runningJob = false;
+
 	/**
 	 * THe constructor for ElevatorJobManagement
+	 *
 	 * @param elevatorId
 	 */
-	public ElevatorJobManagement(int elevatorId ) {
-		currentFloorNumber = 0;
-		elevatorDirection = Direction.IDLE;
+	public ElevatorJobManagement(int elevatorId) {
 		this.elevatorId = elevatorId;
-		readyForJob = true;
-		
-
 	}
 
+	/**
+	 * Add an elevator job
+	 *
+	 * @param elevatorJob the elevator job
+	 */
+	public synchronized void addJob(ElevatorJobMessage elevatorJob) {
+		elevatorJobs.add(elevatorJob);
+		notifyAll();
+	}
 
+	/**
+	 * Return a flag indicating whether the elevator is in an error state
+	 *
+	 * @return true if elevator is in an error state; otherise, return false
+	 */
 	public synchronized boolean isElevatorInError() {
 		notifyAll();
 		return errorState != null;
 	}
 
 	/**
-	 * @return the readyForJob
+	 * Return a flag indicating whether the elevator is ready for a job
+	 *
+	 * @return true if the elevator is ready for a job; otherwise, return false
 	 */
-	public synchronized boolean isReadyForJob() {
+	public synchronized boolean isRunningJob() {
 		notifyAll();
-		return readyForJob;
+		return runningJob;
 	}
-
 
 	/**
-	 * @param readyForJob the readyForJob to set
+	 * Set the running-job flag
+	 *
+	 * @param runningJob a running job flag
 	 */
-	public synchronized void setReadyForJob(boolean readyForJob) {
-		this.readyForJob = readyForJob;
+	public synchronized void setRunningJob(boolean runningJob) {
+		this.runningJob = runningJob;
 		notifyAll();
 	}
 
-
-
+	/**
+	 * Return a flag indicating whether the elevator is at or greater than the job
+	 * Threshold
+	 *
+	 * @return true if the elevator is at or greater than the job Threshold;
+	 *         otherwise, return false
+	 */
 	public synchronized boolean isAtElevatorJobThreshold() {
 		notifyAll();
 		return elevatorJobs.size() >= ELEVATOR_JOB_THRESHOLD;
 	}
 
-
 	/**
+	 * Return the elevator direction
+	 *
 	 * @return the elevatorDirection
 	 */
 	public synchronized Direction getElevatorDirection() {
@@ -79,8 +121,9 @@ public class ElevatorJobManagement {
 		return elevatorDirection;
 	}
 
-
 	/**
+	 * Get the elevator id
+	 *
 	 * @return the elevatorId
 	 */
 	public synchronized int getElevatorId() {
@@ -88,27 +131,30 @@ public class ElevatorJobManagement {
 		return elevatorId;
 	}
 
-
 	/**
-	 * @param errorState the errorState to set
+	 * Set the error state
+	 *
+	 * @param errorState the error state to set
 	 */
 	public synchronized void setErrorState(Exception errorState) {
 		this.errorState = errorState;
 		notifyAll();
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * Get the largest floor destination floor
+	 *
+	 * @return the largest floor destination floor
+	 *
 	 */
 	public synchronized int getLargestDestinationFloor() {
 		if (elevatorJobs.isEmpty()) {
 			return -1;
 		}
-		
+
 		int largestDestinationFloor = elevatorJobs.get(0).getDestinationFloor();
-		
-		for (int i = 0 ; i < elevatorJobs.size(); i++) {
+
+		for (int i = 0; i < elevatorJobs.size(); i++) {
 			if (elevatorJobs.get(i).getDestinationFloor() > largestDestinationFloor) {
 				largestDestinationFloor = elevatorJobs.get(i).getDestinationFloor();
 			}
@@ -116,69 +162,79 @@ public class ElevatorJobManagement {
 		notifyAll();
 		return largestDestinationFloor;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * Get the smallest floor destination floor
+	 *
+	 * @return the smallest floor destination floor
+	 *
 	 */
 	public synchronized int getSmallestDestinationFloor() {
 		if (elevatorJobs.isEmpty()) {
 			return -1;
 		}
-		
+
 		int smallestDestinationFloor = elevatorJobs.get(0).getDestinationFloor();
-		
-		for (int i = 0 ; i < elevatorJobs.size(); i++) {
+
+		for (int i = 0; i < elevatorJobs.size(); i++) {
 			if (elevatorJobs.get(i).getDestinationFloor() < smallestDestinationFloor) {
 				smallestDestinationFloor = elevatorJobs.get(i).getDestinationFloor();
 			}
 		}
 		notifyAll();
 		return smallestDestinationFloor;
-		
+
 	}
-	
-	public synchronized ArrayList<ElevatorJobMessage> getJobsAtFloorNumber(int floorNumber){
-		
+
+	/**
+	 * Get the jobs at a given floor number
+	 *
+	 * @param floorNumber the floor number
+	 * @return the jobs at a given floor number
+	 */
+	public synchronized ArrayList<ElevatorJobMessage> getJobsAtFloorNumber(int floorNumber) {
+
 		ArrayList<ElevatorJobMessage> jobsAtFloorNumber = new ArrayList<>();
-		
-		for (int i = 0 ; i < elevatorJobs.size(); i++) {
+
+		for (int i = 0; i < elevatorJobs.size(); i++) {
 			if (elevatorJobs.get(i).getDestinationFloor() == floorNumber) {
-				jobsAtFloorNumber.add(elevatorJobs.get(i));		}
+				jobsAtFloorNumber.add(elevatorJobs.get(i));
+			}
 		}
 
-		
 		notifyAll();
 		return jobsAtFloorNumber;
 	}
 
-	
 	/**
-	 * 
+	 * Remove the given jobs from the elevator's jobs
+	 *
 	 * @param ElevatorJobMessage
 	 */
-	public void removeJob(ElevatorJobMessage job) {
-		elevatorJobs.remove(job);
+	public synchronized void removeJobs(ArrayList<ElevatorJobMessage> jobs) {
+		elevatorJobs.removeAll(jobs);
 		notifyAll();
 	}
 
-
 	/**
+	 * Get the elevator's current floor number
+	 *
 	 * @return the currentFloorNumber
 	 */
-	public int getCurrentFloorNumber() {
+	public synchronized int getCurrentFloorNumber() {
 		notifyAll();
 		return currentFloorNumber;
-		
+
 	}
 
-
 	/**
+	 * Set the elevator's current floor number
+	 *
 	 * @param currentFloorNumber the currentFloorNumber to set
 	 */
-	public void setCurrentFloorNumber(int currentFloorNumber) {
+	public synchronized void setCurrentFloorNumber(int currentFloorNumber) {
 		this.currentFloorNumber = currentFloorNumber;
 		notifyAll();
 	}
-	
+
 }
