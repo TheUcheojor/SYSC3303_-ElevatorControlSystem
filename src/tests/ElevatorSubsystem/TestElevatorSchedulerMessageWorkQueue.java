5490ee17801a47eb824eb067feaf19970d9264aa
delightoluwayemi <delightoluwayemi@cmail.carleton.ca>
2022-03-25 19:23:53 -0400

    fix: test cases

--- src/tests/ElevatorSubsystem/TestElevatorSchedulerMessageWorkQueue.java
+++ src/tests/ElevatorSubsystem/TestElevatorSchedulerMessageWorkQueue.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ElevatorSubsystem.ElevatorCar;
